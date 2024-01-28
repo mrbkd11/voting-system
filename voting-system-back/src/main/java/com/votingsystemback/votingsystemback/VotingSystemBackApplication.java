636e3576b06688440990ee5c40f13169f3c847a7
mrbkd11 <medbkd11@gmail.com>
2024-01-28 23:17:25 +0100

    setting up env

---
+++ voting-system-back/src/main/java/com/votingsystemback/votingsystemback/VotingSystemBackApplication.java
@@ -0,0 +1,13 @@
+package com.votingsystemback.votingsystemback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSystemBackApplication.class, args);
+	}
+
+}
